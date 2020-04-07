927a02fff699227c595a78df581fc0403da55672
danivichkin <danivichkin@gmail.com>
2020-04-07 09:28:21 +0300

    Added shapes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -16,12 +17,15 @@ import javafx.scene.input.MouseEvent;
 
 import java.io.File;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     double xPIN = 0;
     double yPIN = 0;
+
 
     @FXML
     private ChoiceBox<String> choiceBoxMap;
@@ -73,11 +77,14 @@ public class Controller implements Initializable {
     @FXML
     void plusButton(ActionEvent event) {
 
-        double height = ImageView.getFitHeight();
-        double width = ImageView.getFitWidth();
+        double height = ImageView.getImage().getHeight();
+        double width = ImageView.getImage().getWidth();
+        double scale = 1.02;
 
-        ImageView.setFitHeight(height + 100);
-        ImageView.setFitWidth(width + 100);
+        double newWidth = width - (width * scale - width);
+        double newHeight = height - (height * scale - height);
+
+        ImageView.setViewport(new Rectangle2D(xPIN, yPIN, newWidth, newHeight));
 
     }
 
@@ -89,8 +96,8 @@ public class Controller implements Initializable {
 
         ImageView.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, event -> {
 
-            double xPIN = event.getSceneX();
-            double yPIN = event.getSceneY();
+            xPIN = event.getSceneX();
+            yPIN = event.getSceneY();
 
             if (event.getButton() == MouseButton.PRIMARY) {
                 String label = "Tab has pinned: x: " + xPIN + " y:" + yPIN;
@@ -100,15 +107,11 @@ public class Controller implements Initializable {
 
             if (event.getButton() == MouseButton.SECONDARY) {
                 Double[] cords = new Double[]{xPIN, yPIN};
-                ArrayList<HashMap<Shape, String>> shapes = kostili();
-                for (Map.Entry<Shape, String> entry : shapes.get(0).entrySet()) {
-                    System.out.println(entry.getKey().toString());
-
-                    //TODO
-                    // Видит только первое значение, ноут сел
-                    if (Shape.isDotInSide(entry.getKey(), cords[0], cords[1])) {
-                            Alert alert = new Alert(Alert.AlertType.INFORMATION, entry.getValue());
-                            alert.showAndWait();
+                ArrayList<Shape.ObjectOfShape> shapes = kostili();
+                for (int i = 0; i < shapes.size(); i++) {
+                    if (Shape.isDotInSide(shapes.get(i).getShape(), cords[0], cords[1])) {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION, shapes.get(i).getName());
+                        alert.showAndWait();
                     }
                 }
             }
@@ -118,6 +121,14 @@ public class Controller implements Initializable {
             xCord.setText(String.valueOf(mouseEvent.getSceneX()));
             yCord.setText(String.valueOf(mouseEvent.getSceneY()));
         });
+
+    }
+
+    private void setImageViewToImage() {
+        double width = ImageView.getFitWidth();
+        double height = ImageView.getFitHeight();
+        double scale = 1.0002;
+        ImageView.setViewport(new Rectangle2D(xPIN, yPIN, width * scale, height * scale));
 
     }
 
@@ -137,25 +148,24 @@ public class Controller implements Initializable {
         return (fullUrl[fullUrl.length - 1]);
     }
 
-    private ArrayList<HashMap<Shape, String>> kostili() {
-        ArrayList<HashMap<Shape, String>> arrayList = new ArrayList<>();
-        HashMap<Shape, String> hashMap = new HashMap();
+    private ArrayList<Shape.ObjectOfShape> kostili() {
+        ArrayList<Shape.ObjectOfShape> arrayList = new ArrayList<>();
 
         Shape.Point point0 = new Shape.Point(76.0, 428.0);
         Shape.Point point1 = new Shape.Point(66.0, 176.0);
         Shape.Point point2 = new Shape.Point(183.0, 213.0);
         Shape.Point point3 = new Shape.Point(251.0, 359.0);
         Shape shapeKazan = new Shape(point0, point1, point2, point3);
-        hashMap.put(shapeKazan, "Kazan");
+        Shape.ObjectOfShape objectOfShapeKazan = new Shape.ObjectOfShape(shapeKazan, "Kazan");
+        arrayList.add(objectOfShapeKazan);
 
         Shape.Point point10 = new Shape.Point(223, 235);
         Shape.Point point11 = new Shape.Point(224, 190);
         Shape.Point point12 = new Shape.Point(267, 185);
         Shape.Point point13 = new Shape.Point(254, 234);
-        Shape shapeDerbishki = new Shape(point0, point1, point2, point3);
-        hashMap.put(shapeDerbishki, "Derbishki");
-
-        arrayList.add(hashMap);
+        Shape shapeDerbishki = new Shape(point10, point11, point12, point13);
+        Shape.ObjectOfShape objectOfShapeDerbishki = new Shape.ObjectOfShape(shapeDerbishki, "Derbishki");
+        arrayList.add(objectOfShapeDerbishki);
 
         return arrayList;
     }
